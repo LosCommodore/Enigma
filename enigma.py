1f433c27a1e8aa39231a4720994e9309fd5b4445
Cla <>
2019-08-25 21:53:42 +0200

    first rotation logic

--- enigma.py
+++ enigma.py
@@ -21,12 +21,13 @@ class Scrambler():
 
 class  Rotor(Scrambler):
 
-    def __init__(self, name:str, wiring: str):
+    def __init__(self, name:str, wiring: str, notches=tuple(), isStatic=False):
         super().__init__(name);
 
         # Check if wiring is valid: is alphabet and unique items
         assert tuple(sorted(wiring)) == alphabet ,'Rotor falsch initialisiert, falsches Alphabet !'
-        
+        #//TODO: char is not allowed to map on itself ?
+
         # save wiring as Dict
         self.wiring     = {k:v for k,v in zip(alphabet,wiring)} 
         
@@ -37,19 +38,37 @@ class  Rotor(Scrambler):
         numWiring = [ord(a) for a in wiring]
         self.inv_mapping = np.argsort(numWiring) - np.arange(len(alphabet))
         
-        self.rotation = 0 
-
+        self.notches = tuple(ord(x)-ord('a') for x in notches)
+        
+        self.ringPos = 0
+        self.isStatic = isStatic
+        self.__rotation = 0 
     def route(self, character: int) -> int:
-        return (character + self.mapping[(character + self.rotation) % len(alphabet)]) % len(alphabet)
+
+        rot = self.ringPos + self.rotation
+        return (character + self.mapping[(character + rot) % len(alphabet)]) % len(alphabet)
 
     def inv_route(self, character):
-        return (character + self.inv_mapping[(character + self.rotation) % len(alphabet)]) % len(alphabet)
+        rot = self.ringPos + self.rotation
+        return (character + self.inv_mapping[(character + rot) % len(alphabet)]) % len(alphabet)
+
+    def doesStep(self):
+        return any([self.rotation == n for n in self.notches])
+
+    @property 
+    def rotation(self):
+        return self.__rotation
+
+    @rotation.setter 
+    def rotation(self,value):
+        self.__rotation = value % len(alphabet)
+
 
 class PlugBoard(Scrambler):
     def __init__(self,name):
         super().__init__(name);
         self.__cables = [] #List of Tuples, e.g: cables = [('a','b'), ('u','v'), ('r','x'), ('t','w')]
-    pass
+        self.mapping = [ord(x)-ord('a') for x in alphabet]
 
     @property 
     def cables(self): 
@@ -69,7 +88,7 @@ class PlugBoard(Scrambler):
             plugMapping[i1] = cable[0]
 
         self.mapping = [ord(x)-ord('a') for x in plugMapping]
-    
+
     def route(self, character: int) -> int:
         return self.mapping[character]
 
@@ -93,6 +112,11 @@ class Enigma:
         assert key in alphabet, 'ungültiger Schlüssel!'
         key = ord(key) - ord('a')
 
+        #rotate
+        self.rotate()
+
+        #calc Wiring
+
         routing = [] # --> names
         char = [key] # --> [0, 1, 10, 11, 21, 22, 17, 6, 5, 5]
         for scram in self.scramblers:
@@ -104,7 +128,20 @@ class Enigma:
             char.append(scram.inv_route(char[-1]))
 
         return char, routing
-  
+    
+    def rotate(self):
+        rotors = [x for x in self.scramblers if isinstance(x,Rotor) and not x.isStatic]
+        
+        doRotate= [False] * len(rotors)
+        doRotate[0] = True 
+        for i,step in enumerate([s.doesStep() for s in rotors]):
+            if step:
+                doRotate[i] = True
+                doRotate[i+1] = True
+
+        for rotor,step in zip(rotors,doRotate):
+            rotor.rotation += step
+        
 #%% main
 
 #Create Enigma
@@ -112,28 +149,32 @@ riddle = Enigma()
 
 #Create Plugboard
 plugBoard = PlugBoard('Plugboard')
-plugBoard.cables =  [('a','b'), ('u','v'), ('r','x'), ('t','w')]
+#plugBoard.cables =  [('a','b'), ('u','v'), ('r','x'), ('t','w')]
 #plugBoard.print()
 
 #Create Rotors
-rot1 = Rotor('I','EKMFLGDQVZNTOWYHXUSPAIBRCJ'.lower())
-rot2 = Rotor('II','AJDKSIRUXBLHWTMCQGZNPYFVOE'.lower())
-rot3 = Rotor('III','BDFHJLCPRTXVZNYEIWGAKMUSQO'.lower())
-ukw_b = Rotor('ukw_b','YRUHQSLDPXNGOKMIEBFZCWVJAT'.lower()) 
-
+rot1 = Rotor('I','EKMFLGDQVZNTOWYHXUSPAIBRCJ'.lower(),('q',))
+rot2 = Rotor('II','AJDKSIRUXBLHWTMCQGZNPYFVOE'.lower(),('e',))
+rot3 = Rotor('III','BDFHJLCPRTXVZNYEIWGAKMUSQO'.lower(),('v',))
+ukw_b = Rotor('ukw_b','YRUHQSLDPXNGOKMIEBFZCWVJAT'.lower(),isStatic=True) 
+                      
 #Wire Enigma:
 riddle.scramblers.append(plugBoard)
 riddle.scramblers += [rot1, rot2, rot3, ukw_b]
 
 #Press Key:
-key = 'a'
-char,routing = riddle.pressKey(key) 
-print(char,routing)
+text = 'aaa'
 
-lastChar = chr(char[-1]+ord('a'))
+encoded=[]
+for key in text:
+    char,routing = riddle.pressKey(key) 
+    print(char,routing)
+    encoded.append(chr(char[-1]+ord('a')))
 
-charInv,routingInv = riddle.pressKey(lastChar)
-print(charInv,routingInv)
+print("Encoded Text: " , encoded)
+
+#charInv,routingInv = riddle.pressKey(lastChar)
+#print(charInv,routingInv)
 
 print("ende")
 
