34de0bcd71afed8fbf0b281140d9d9523fa3f9fa
ChLan <>
2021-11-07 11:56:57 +0100

    refactoring

--- test/test.py
+++ test/test.py
@@ -1,10 +1,10 @@
 import pytest
-
 import enigmatic
+import random
 
 
 def test_plug_board():
-    cables = ('bz','fg')
+    cables = ('bz', 'fg')
 
     p = enigmatic.PlugBoard(tuple())
     assert p.route(0) == 0
@@ -12,28 +12,28 @@ def test_plug_board():
 
     p.cables = cables
     for cable in cables:
-        i,o = enigmatic._letters2num(cable)
+        i, o = enigmatic._letters2num(cable)
         assert p.route(i) == o
         assert p.route(o) == i
 
 
 def test_unvalid_rotor_spec():
     with pytest.raises(Exception):
-        spec = enigmatic.RotorSpec("testRotor", "ABC", (), False)
+        enigmatic.RotorSpec("testRotor", "ABC", (), False)
 
 
 def test_create_rotor_spec():
     spec = enigmatic.RotorSpec('I', 'EKMFLGDQVZNTOWYHXUSPAIBRCJ'.lower(), ('q',), False)
     print(spec)
 
-#
-# def test_rotor_symmetry():
-#     wiring = list(enigma.ALPHABET)
-#     random.shuffle(wiring)
-#     wiring = "".join(str(x) for x in wiring)
-#     r = enigma.Rotor("r1", wiring, (), False)
-#
-#     key_in = 5
-#     key_out = r.inv_route(r.route(key_in))
 
-#    assert key_in == key_out
+def test_rotor_symmetry():
+    wiring = list(enigmatic.ALPHABET)
+    random.shuffle(wiring)
+    wiring = "".join(str(x) for x in wiring)
+    spec = enigmatic.RotorSpec("r1", wiring, (), False)
+    r = enigmatic.Rotor(spec)
+
+    for i,_ in enumerate(enigmatic.ALPHABET):
+        letter_out = r.inv_route(r.route(i))
+        assert i == letter_out
