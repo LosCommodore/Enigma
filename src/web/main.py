994c22fc5bf754ae4f0d372d86ccdfd211ada187
LosCommodore <60575868+LosCommodore@users.noreply.github.com>
2024-11-16 20:17:08 +0100

    web

--- src/web/main.py
+++ src/web/main.py
@@ -2,6 +2,9 @@ from enigmatic.enigma import Enigma
 from pyscript import window, document  # noqa: F401
 
 def handle_click(event):
+    sel_output = document.querySelector("#decode")
+    sel_output.innerHTML = ""
+
     rot_names = [document.querySelector(f"#rot{i+1}").value for i in range(5)]
     window.console.log(f"{rot_names=}")
 
@@ -21,5 +24,4 @@ def handle_click(event):
     input_text = document.querySelector("#cyper").value
     output_text = enigma.write(input_text)
 
-    sel_output = document.querySelector("#decode")
     sel_output.innerHTML = output_text
