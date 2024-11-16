e480fd07042034cd09fe723c3f400dff256166e5
LosCommodore <60575868+LosCommodore@users.noreply.github.com>
2024-11-16 09:58:29 +0100

    web app

--- src/web/main.py
+++ src/web/main.py
@@ -1,24 +1,29 @@
 from enigmatic.enigma import Enigma
-from pyscript import window, document
+from pyscript import window, document  # noqa: F401
 
-enigma = Enigma.assemble(wheel_specs=["ukw_caesar", "beta", "V", "VI", "VIII"])
-enigma.plug_board.add_cables("AE BF CM DQ HU JN LX PR SZ VW")
-enigma.wheel_positions = "*SCHL"
-enigma.ring_positions = "*AAEL"
+whl_input = document.querySelector("#whl_pos")
+ring_input = document.querySelector("#ring_pos")
 
-cypher_text = """TAWZZYNZGGMICVLSADLQFDFKHDGZLSEEMDFFBQLYISEIWKDONDWJWETKZOWWYLTYII
-OWSMCFTGNLKOZCAAWHBHFJDRWLDHFHEWUBPWVDEZFPBOCVKECVFXWZGSVBDPHLTVOJCFLSQNSODYFMBQFQU
-LTHDISGOQURABEHHIBNXNAAKLEJMXQHJWXAVDLVTPTSTDCPWPYXCUVWAFJTTSFUSPTHOSTHXADWRQWZJCTU
-KWCWHYFYQTRTPKPPJGPMDZQDAXAIZLWCADYZUTLXJJUDQGMFWZRWZXBQRHNCRUFUZZ.LALDHHOIBWYPKTSR
-NQUHMUPTFWUHWBIKMASACVPRELBRRMRKLZWLVSFGUQFFXTAVQIHXDXRKQVGARLMZDBMDGRYNWIYSHWCNHIW
-XYFBFGXXQNPTSEROCVYTOJTKKLQRALUDQMUFAKRHKOJCMWPSZHNAVVSEWFFPGRJTVNOLSGRERKXSCYWBOQT
-PXOXJLQXSPYTMEOXOURSHIBYBNWQEFPTGGMCVXNGCDFQIYLCMPDLWOOAALODSWCDAWOZOMBGDZOQKZWQKFR
-MRWCHGWFUERWPJAQFDRBVOAEYYAIYNPCLJDXOCBAZOHQOKVUFGLQNXQYDUZJIFXRVJDQ.OOOGXIGNONPEQL
-KQBTVJ.DANIXQ.FDYXRDTQTFP.ZEHEOMXPPPFGGLTVFODHBSQPAQMDDWXMMGAJZVURSIYGAAXDZDV""".replace(".", "")
 
-c_el = document.querySelector("#cyper")
-c_el.innerHTML = cypher_text
+cables_input = document.querySelector("#cables")
+cables_input.value = "AE BF CM DQ HU JN LX PR SZ VW"
 
-output_text = enigma.write(cypher_text)
-c_el = document.querySelector("#decode")
-c_el.innerHTML = output_text
+whl_input.value = "*SCHL"
+ring_input.value = "*AAEL"
+
+def handle_click(event):
+    input_text = document.querySelector("#cyper").value
+    whl_text = document.querySelector("#whl_pos").value
+    ring_text = document.querySelector("#ring_pos").value
+
+    # window.console.log(f"{input_text=}")
+
+    enigma = Enigma.assemble(wheel_specs=["ukw-c", "I", "V", "VI", "VIII"])
+    enigma.plug_board.add_cables("AE BF CM DQ HU JN LX PR SZ VW")
+    enigma.wheel_positions = whl_text
+    enigma.ring_positions = ring_text
+
+    output_text = enigma.write(input_text)
+
+    sel_output = document.querySelector("#decode")
+    sel_output.innerHTML = output_text
